2dd849c24ba63ad05e335f773603fe72e98bbc26
Adam Fitzgerald <afitz@gatech.edu>
2010-09-21 21:52:38 -0400

    cleaned up some warnings - may have introduced silencing of legitimately dangerous things

--- lua51/golua.h
+++ lua51/golua.h
@@ -1,8 +1,7 @@
-//#include "_cgo_export.h"
 
 typedef struct { void *t; void *v; } GoInterface;
 
-//function to setup metatables, etc
+/* function to setup metatables, etc */
 void clua_initstate(lua_State* L);
 
 unsigned int clua_togofunction(lua_State* L, int index);
@@ -13,8 +12,3 @@ GoInterface clua_atpanic(lua_State* L, unsigned int panicf_id);
 int clua_callluacfunc(lua_State* L, lua_CFunction f);
 lua_State* clua_newstate(void* goallocf);
 void clua_setallocf(lua_State* L, void* goallocf);
-
-
-//TODO: get/set panicf
-//TODO: get/set allocf
-//TODO: userdata support
