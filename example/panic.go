e44d144d88f109a8a9252ed6eab9a93102d6d5f7
aarzilli <alessandro.arzilli@gmail.com>
2012-06-26 20:00:27 +0200

    changed examples to make them easier to run locally

--- example/panic.go
+++ example/panic.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/aarzilli/golua/lua"
+import "../lua"
 import "fmt"
 
 func test(L *lua.State) int {
--- example/userdata.go
+++ example/userdata.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/aarzilli/golua/lua"
+import "../lua"
 import "unsafe"
 import "fmt"
 
